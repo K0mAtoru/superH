9d611d1dc7815a4b650b48586bde68b8dea01578
K0mAtoru <mctj1218@gmail.com>
2016-03-09 23:44:33 +0900

    p18-20: tuple

---
+++ chap01/tuple.hs
@@ -0,0 +1,7 @@
+
+main = do
+    print $ (1, 4)
+    print $ (3, 'a', "hello")
+    print $ (50, 50.4, "hello", 'b')
+    -- error print $ [(1,2),(8,11,5),(4,5)]
+    -- error print $ [(1,2),("onece",2)]
