f71048d1bd75fef2bd3184a2181dd437c10a47a8
utam0k <utam0k.k0ma@gmail.com>
2016-04-20 16:41:09 +0900

    p112-117: Algebraic Data Types Intro.

--- chap07/Shapes.hs
+++ chap07/Shapes.hs
@@ -1,9 +1,5 @@
--- data Shape = Circle Float Float Float | Rectangle Float Float Float Float deriving (Show)
-
--- area :: Shape -> Float
--- area (Circle _ _ r) = pi * r ^ 2
--- area (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
-
+module Shapes
+( Point, Shape, area, nudge, baseCircle, baseRect) where
 data Point = Point Float Float deriving (Show)
 data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
 
@@ -20,14 +16,3 @@ baseCircle r = Circle (Point 0 0) r
 
 baseRect :: Float -> Float -> Shape
 baseRect width height = Rectangle (Point 0 0) (Point width height)
-
-main :: IO ()
-main = do
-    -- print $ area $ Circle 10 20 10
-    -- print $ area $ Rectangle 0 0 100 100
-    -- print $ Circle 10 20 5
-    -- print $ map (Circle 10 20) [4,5,6,6]
-    print $ area (Rectangle (Point 0 0) (Point 100 100))
-    print $ area (Circle (Point 0 0) 24)
-    print $ nudge (Circle (Point 34 34) 10) 5 10
-    print $ nudge (baseRect 40 100) 60 23
