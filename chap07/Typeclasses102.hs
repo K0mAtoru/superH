dc8b5012cbc6c2425da102d9cbb251241ef52c4f
utam0k <utam0k.k0ma@gmail.com>
2016-05-02 16:23:47 +0900

    p142-148: Type classes 102.

---
+++ chap07/Typeclasses102.hs
@@ -0,0 +1,34 @@
+{-
+class Eq a where
+    (==) :: a -> a -> Bool
+    (/=) :: a -> a -> Bool
+    x == y = not (x /= y)
+    x /= y = not (x == y)
+-}
+
+data TrafficLight = Red | Yellow | Green
+
+instance Eq TrafficLight where
+    Red    == Red    = True
+    Green  == Green  = True
+    Yellow == Yellow = True
+    _      == _      = False
+
+instance Show TrafficLight where
+    show Red = "Red light"
+    show Yellow = "Yellow light"
+    show Green = "Green light"
+
+{-
+class (Eq m) => Eq (Maybe m) where
+    Just x == Just y = x == y
+    Nothing == Nothing = True
+    _ == _ = False
+-}
+
+main :: IO ()
+main = do
+    print $ Red == Red
+    print $ Red == Yellow
+    print $ Red `elem` [Red, Yellow, Green]
+    print $ [Red, Yellow, Green]
